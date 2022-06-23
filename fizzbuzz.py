6b41c3cfae288b42cfc85749dd5a336c4ba1ad92
jbkim0530 <jbkim0530@gmail.com>
2022-06-23 20:47:54 +0900

    feat: FizzBuzz Done

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,23 @@
 FizzBuzz Game
 
 # 3의 배수 : 'fizz' (Done)
-# 5의 배수 : 'buzz'
-# 15의 배수 : 'fizzbuzz'
+# 5의 배수 : 'buzz'(Done)
+# 15의 배수 : 'fizzbuzz'(Done)
 # 나머지 : 숫자(1-45) (Done)
 
 
 for i in range(1, 46):
-    if i % 3 == 0
-    print('fizz')
-
-    elif i % 5 == 0
-    print('buzz')
     
+    if i % 15 == 0:
+        print('FizzBuzz')
+
+    elif i % 3 == 0:
+        print('fizz')
+
+    elif i % 5 == 0:
+        print('buzz')
+    
+    else:
+        print(i)
+
+
