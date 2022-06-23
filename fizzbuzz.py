2dd2d5bdbea7da814f6dc02cbe94b70911d64e24
jbkim0530 <jbkim0530@gmail.com>
2022-06-23 20:32:03 +0900

    feat: Print buzz if is times of 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 FizzBuzz Game
 
-# 3의 배수 : 'fizz'
+# 3의 배수 : 'fizz' (Done)
 # 5의 배수 : 'buzz'
 # 15의 배수 : 'fizzbuzz'
 # 나머지 : 숫자(1-45) (Done)
@@ -8,5 +8,8 @@ FizzBuzz Game
 
 for i in range(1, 46):
     if i % 3 == 0
-    print(i)
+    print('fizz')
+
+    elif i % 5 == 0
+    print('buzz')
     
